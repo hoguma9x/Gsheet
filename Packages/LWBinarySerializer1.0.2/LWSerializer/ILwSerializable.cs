dde52da7be7a9478d18d121a71e760c8090060dc
MERBOOK <eowns9753@naver.com>
2026-09-04 16:28:23 +0900

    실험적, LwSerializer 의존성 분리 테스트

--- Packages/LWBinarySerializer1.0.2/LWSerializer/ILwSerializable.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LWSerializer
-{
-    public interface ILwSerializable
-    {
-        void OnNativeWrite(LwBinaryWriter writer);
-        void OnNativeRead(LwBinaryReader reader);
-    }
-}
